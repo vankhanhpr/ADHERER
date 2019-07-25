6fb1d226eb79c600ef22c97f3bb1e63ddb1793e5
Nguyễn Văn Khánh <donkihote2305@gmail.com>
2019-07-25 22:29:31 +0700

    home-dangvien 25.7

---
+++ source/Adherer/WebClient/wwwroot/js/admin/addangbo.js
@@ -0,0 +1,16 @@
+﻿
+var bol = true;
+function toggeChibo(obj) {
+    if (bol) {
+        $(".f-chibo").show(400);
+        $(obj).removeClass("fa-plus");
+        $(obj).addClass("fa-minus");
+        bol = false;
+    }
+    else {
+        $(".f-chibo").hide(400);
+        $(obj).addClass("fa-plus");
+        $(obj).removeClass("fa-minus");
+        bol = true;
+    }
+}
